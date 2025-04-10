5190e58a8e6c114c2dfdfa9d1ddab8b41ff6495e
Lisa Carvalho <i.m.lisaaa14@gmail.com>
2025-04-10 17:40:06 +0100

    Verified and fixed GameUnit

--- GameUnit/MilitaryUnit.cs
+++ GameUnit/MilitaryUnit.cs
@@ -4,12 +4,13 @@ namespace GameUnit
     {
         public int AttackPower { get; }
         public int XP { get; private set; }
+        public override int Health { get => base.Health+XP; }
+
 
         public MilitaryUnit(int mov, int health, int attackPower) : base(mov, health)
         {                                                        // Unit(int, int)
             AttackPower = attackPower;
             XP = 0;
-            Health = Health + XP;
         }
 
         public override float Cost
--- GameUnit/SettlerUnit.cs
+++ GameUnit/SettlerUnit.cs
@@ -2,18 +2,8 @@ namespace GameUnit
 {
     public class SettlerUnit : Unit
     {
-        public SettlerUnit(int mov, int health) : base(mov, health)
-        {
-            mov = 1;
-            health = 3;
-        }
+        public SettlerUnit() : base(1, 3) { }
 
-        public override float Cost
-        {
-            get
-            {
-                return 5f;
-            }
-        }
+        public override float Cost => 5;
     }
 }
